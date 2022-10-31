3ee673943583fb8777d71915ab4d9baab6985af5
Bachuchee <gal.aloni@outlook.com>
2022-10-31 21:58:22 +0200

    Improved adaptability between platforms

--- anylearn/lib/views/login/login.dart
+++ anylearn/lib/views/login/login.dart
@@ -13,29 +13,42 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Image(
-          image: AssetImage('assets/images/logo.png'),
+          image: AssetImage('assets/images/Logo.png'),
           width: 250.0,
           height: 250.0,
         ),
         backgroundColor: Colors.white,
       ),
-      body: Row(
-        children: const [
-          Expanded(
-            flex: 1,
-            child: LoginSection(),
-          ),
-          Expanded(
-            flex: 2,
-            child: Image(
-              image: AssetImage('assets/images/login_splash.jpg'),
-              fit: BoxFit.fitHeight,
-            ),
-          ),
-        ],
-      ),
+      body: (() {
+        if (MediaQuery.of(context).size.width > 1200.0) {
+          return Row(
+            children: [
+              const Expanded(
+                flex: 1,
+                child: LoginSection(),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/login_splash.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        } else {
+          return const LoginSection();
+        }
+      }()),
     );
   }
 }
