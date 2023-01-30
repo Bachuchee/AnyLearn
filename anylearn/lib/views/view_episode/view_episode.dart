41b07766ba7d1e792624ba970eef81136c8ba98c
Bachuchee <gal.aloni@outlook.com>
2023-01-30 12:47:52 +0200

    Finish creating the base view_episode page (except for one annoying bug)

--- anylearn/lib/views/view_episode/view_episode.dart
+++ anylearn/lib/views/view_episode/view_episode.dart
@@ -1,10 +1,12 @@
 import 'package:anylearn/models/episode.dart';
-import 'package:anylearn/views/view_episode/components/videoplayer.dart';
+import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pod_player/pod_player.dart';
+import 'package:video_player/video_player.dart';
 
 import '../../Theme/colors.dart';
 import '../../models/pocket_client.dart';
@@ -14,9 +16,9 @@ import '../view_course/view_course.dart';
 final episodeProvider = StateProvider<Episode>((ref) => Episode());
 
 class ViewEpisode extends ConsumerStatefulWidget {
-  ViewEpisode({super.key, this.episodeId = ""});
+  const ViewEpisode({super.key, this.episodeId = ""});
 
-  String episodeId;
+  final String episodeId;
 
   @override
   ConsumerState<ViewEpisode> createState() => _ViewEpisodeState();
@@ -25,21 +27,40 @@ class ViewEpisode extends ConsumerStatefulWidget {
 class _ViewEpisodeState extends ConsumerState<ViewEpisode> {
   final _client = PocketClient.client;
 
-  Future<void> getEpisode() async {
+  late final Future<void> _initVid;
+
+  late final PodPlayerController _podController;
+
+  Future<void> setUpData() async {
     ref.read(episodeProvider.notifier).state =
         await PocketClient.getEpisode(widget.episodeId);
+
+    final episode = ref.watch(episodeProvider);
+
+    _podController = PodPlayerController(
+      playVideoFrom: PlayVideoFrom.network(
+        _client.getFileUrl(episode.episodeModel!, episode.videoName).toString(),
+      ),
+    )..initialise();
   }
 
   @override
   void initState() {
     super.initState();
 
-    getEpisode();
+    _initVid = setUpData();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _podController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final episode = ref.watch(episodeProvider);
+    final md = MediaQuery.of(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -68,18 +89,69 @@ class _ViewEpisodeState extends ConsumerState<ViewEpisode> {
           ),
         ],
       ),
-      body: Column(
+      body: ListView(
         children: [
           Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: episode.videoName != ""
-                ? AnyLearnVideoPlayer(
-                    _client
-                        .getFileUrl(episode.episodeModel!, episode.videoName)
-                        .toString(),
-                  )
-                : const CircularProgressIndicator(color: secondaryColor),
+            padding: const EdgeInsets.all(8.0),
+            child: FutureBuilder(
+              future: _initVid,
+              builder: (context, snapshot) => snapshot.connectionState ==
+                      ConnectionState.done
+                  ? PodVideoPlayer(
+                      controller: _podController,
+                      videoThumbnail: DecorationImage(
+                        image: NetworkImage(
+                          _client
+                              .getFileUrl(
+                                  episode.episodeModel!, episode.thumbnailName)
+                              .toString(),
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                      podProgressBarConfig: const PodProgressBarConfig(
+                        backgroundColor: secondarySurface,
+                        playingBarColor: secondaryColor,
+                        circleHandlerColor: secondaryColor,
+                        circleHandlerRadius: 0.0,
+                      ),
+                    )
+                  : Container(
+                      height: md.size.height * 1.1,
+                      width: md.size.width * 0.4,
+                      color: Colors.grey,
+                    ),
+            ),
           ),
+          ListTile(
+            title: Text(
+              "Chapter #${episode.episodeNumber}",
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.6),
+                fontSize: 14.0,
+              ),
+            ),
+            subtitle: Text(
+              episode.title,
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  episode.description,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
