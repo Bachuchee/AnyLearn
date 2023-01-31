88404c3e2e9744f1d46b9d751bf7b696af3bbb95
Bachuchee <81419712+Bachuchee@users.noreply.github.com>
2023-01-31 19:08:15 +0200

    V3.0.0/add watch status (#12)
    * Fix bugs caused by PR
    
    * Enable saving user's watch status
    
    * Add a 'Continue Watching' feature
    
    * Add the option to view all of the user's ongoing courses

--- anylearn/lib/models/pocket_client.dart
+++ anylearn/lib/models/pocket_client.dart
@@ -1,13 +1,14 @@
-
+import 'package:anylearn/controllers/duration_service.dart';
 import 'package:anylearn/models/episode.dart';
 import 'package:anylearn/models/topic.dart';
 import 'package:anylearn/models/user.dart';
+import 'package:anylearn/models/view_status.dart';
+import 'package:anylearn/views/view_course/view_course.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 import 'package:pocketbase/pocketbase.dart';
 
 import 'course.dart';
-
 
 class PocketClient {
   static final _client = PocketBase('http://127.0.0.1:8090');
@@ -45,6 +46,28 @@ class PocketClient {
     final episodeModel = await _client.collection('episodes').getOne(id);
     final course = await getCourseById(episodeModel.data['course_id']);
     return Episode.fromJson(episodeModel.data, episodeModel, course);
+  }
+
+  static Future<List<Course>> getOngoingCourses(Topic? topicFilter) async {
+    try {
+      final List<Course> courses = [];
+      final courseList = await _client
+          .collection('course_status')
+          .getList(filter: 'user_id = "${model.id}"');
+      for (var courseModel in courseList.items) {
+        final curCourse = await getCourseById(courseModel.data['course_id']);
+        final topicNames = <String>[];
+        for (var element in curCourse.topics) {
+          topicNames.add(element.name);
+        }
+        if (topicFilter == null || topicNames.contains(topicFilter.name)) {
+          courses.add(curCourse);
+        }
+      }
+      return courses;
+    } catch (e) {
+      return [];
+    }
   }
 
   static Future<List<Course>> getCourses(Topic? topicFilter) async {
@@ -161,6 +184,72 @@ class PocketClient {
     } catch (e) {
       return Course();
     }
+  }
+
+  static Future<ViewStatus> getSavedPosition(
+    String userId,
+    String courseId,
+    int epNumber,
+  ) async {
+    try {
+      final statusList = await _client.collection("course_status").getList(
+            filter: 'user_id = "$userId" && course_id = "$courseId"',
+            sort: "-created",
+          );
+
+      if (statusList.items.length > 1) {
+        for (var item in statusList.items.sublist(1)) {
+          _client.collection('course_status').delete(item.id);
+        }
+      }
+      final status = statusList.items[0];
+
+      if (status.data["current_episode"] != epNumber) {
+        return ViewStatus(status.data["current_episode"], Duration.zero);
+      }
+      print("got here bro");
+      return ViewStatus(epNumber, parseDuration(status.data["position"]));
+    } catch (e) {
+      print("failed");
+      return const ViewStatus(-1, Duration.zero);
+    }
+  }
+
+  static Future<void> updateWatchStatus(
+    String courseId,
+    String userId,
+    Duration pos,
+    int epNumber,
+    bool hasBeenCreated,
+  ) async {
+    try {
+      final status = await _client.collection("course_status").getList(
+          filter: 'user_id = "$userId" && course_id = "$courseId"',
+          sort: "-created");
+
+      if (status.items.length > 1) {
+        for (var item in status.items.sublist(1)) {
+          _client.collection('course_status').delete(item.id);
+        }
+      }
+      if (status.items.isEmpty || !hasBeenCreated) {
+        final body = {
+          "user_id": userId,
+          "course_id": courseId,
+          "current_episode": epNumber,
+          "position": pos.toString(),
+        };
+        await _client.collection("course_status").create(body: body);
+      } else {
+        final body = {
+          "current_episode": epNumber,
+          "position": pos.toString(),
+        };
+        await _client
+            .collection("course_status")
+            .update(status.items[0].id, body: body);
+      }
+    } catch (e) {}
   }
 
   static Future<bool> createCourse(
